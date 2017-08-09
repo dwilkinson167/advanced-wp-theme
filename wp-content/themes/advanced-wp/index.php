530e4c2ad6d1d5ffb242c44b4c024edd73500f0f
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-08-08 19:48:59 -0700

    made dynamic for wp

--- wp-content/themes/advanced-wp/index.php
+++ wp-content/themes/advanced-wp/index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-	<title>Advanced WP Theme</title>
-	<link rel="stylesheet" href="style.css">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"
+	<title><?php bloginfo( 'name' ); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header>
 	<div class="container">
 		<h1>
 			<a href="index.html">
-				Advanced WordPress Theme
+				<?php bloginfo( 'name' ); ?>
 			</a>
-			<small>Another WordPress Theme</small>
+			<small><?php bloginfo( 'description' ); ?></small>
 		</h1>
 		<div class="h_right">
-			<form>
-				<input type="text" placeholder="Search...">
+			<form method="get" action="<?php esc_url( home_url( '/' ) ); ?>">
+				<input type="text" name="s" placeholder="Search...">
 			</form>
 		</div>
 	</div>
@@ -23,11 +26,12 @@
 
 <nav class="nav main-nav">
 	<div class="container">
-		<ul>
-			<li><a href="index.html">Home</a></li>
-			<li><a href="about.html">About</a></li>
-			<li><a href="#">Services</a></li>
-		</ul>
+		<?php
+		$args = array(
+			'theme_location' => 'primary'
+		); ?>
+
+		<?php wp_nav_menu( $args ); ?>
 	</div>
 </nav>
 
@@ -95,8 +99,11 @@
 	</div>
 
 </footer>
+<?php wp_footer(); ?>
 </body>
 </html>
+
+
 
 
 
